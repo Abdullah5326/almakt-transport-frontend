75671952d9c31b81c7f3438337e70b0c49be2ec6
Abdullah5326 <abdullahweb53264189@gmail.com>
2026-03-21 20:23:54 +0500

    initial commit

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,15 @@
+function Layout() {
+  return (
+    <div className="h-dvh flex">
+      <sidebar className="bg-white w-50 ">
+        <h1>Sidebar</h1>
+      </sidebar>
+      <div className="flex flex-col w-full">
+        <nav className="">Nav</nav>
+        <main className="bg-stone-100 h-full w-full ">Main content</main>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
