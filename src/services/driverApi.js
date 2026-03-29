12f38e0e2613141f2c1d7d5b670db1dc6a59c854
Abdullah5326 <abdullahweb53264189@gmail.com>
2026-03-29 13:51:42 +0500

    Changed the api's url to cloud url

--- src/services/driverApi.js
+++ src/services/driverApi.js
@@ -2,7 +2,7 @@ const LOCAL_URL = "http://localhost:3000/api/v1";
 const CLOUD_URL = "https://almakt-transport-backend.onrender.com/api/v1";
 
 export async function getAllDriver() {
-  const res = await fetch(`${LOCAL_URL}/drivers`);
+  const res = await fetch(`${CLOUD_URL}/drivers`);
 
   const data = await res.json();
   if (!res.ok) throw new Error("There is error in fetching drivers");
